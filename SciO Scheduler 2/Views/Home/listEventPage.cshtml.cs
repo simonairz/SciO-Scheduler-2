f16131a6b4ab1c6ddf00f707266929bfe609759e
simon <simon@LAPTOP-0H2HKIN6>
2022-01-13 11:16:07 -0600

    Add project files.

---
+++ SciO Scheduler 2/Views/Home/listEventPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SciO_Scheduler_2.Views.Home
+{
+    public class listEventPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
